49fc2454f3a7eb377152f4e7a8593dd3761c6d28
Arif <159087175+md-arif-hasnat@users.noreply.github.com>
2026-09-24 15:21:30 +0200

    Keep current app version at 1.0.33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "secure.kidsguard.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 34
-        versionName = "1.0.34"
+        versionCode = 33
+        versionName = "1.0.33"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
