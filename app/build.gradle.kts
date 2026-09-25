1c32253f419652f0fd0109fa2427effd86e33b26
Arif <159087175+md-arif-hasnat@users.noreply.github.com>
2026-09-25 02:37:51 +0200

    Bump Android test version to 1.0.43

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "secure.kidsguard.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 42
-        versionName = "1.0.42"
+        versionCode = 43
+        versionName = "1.0.43"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
