ede6073fd29c27fe74ae038cff2da02099aba7b7
Arif <159087175+md-arif-hasnat@users.noreply.github.com>
2026-09-26 05:46:48 +0200

    chore: bump Android version to 1.0.52

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "secure.kidsguard.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 51
-        versionName = "1.0.51"
+        versionCode = 52
+        versionName = "1.0.52"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
